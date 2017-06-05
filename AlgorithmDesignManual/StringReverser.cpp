2330cbf602dc5170ce8210f1b6e010443bec0627
Josh Varty <joshvarty@gmail.com>
2017-06-05 01:34:39 -0700

    Correction + setup

--- AlgorithmDesignManual/StringReverser.cpp
+++ AlgorithmDesignManual/StringReverser.cpp
@@ -3,13 +3,15 @@
 
 std::string StringReverser::Reverse(std::string input) {
 
+	//First reverse the entire string
 	int length = input.length();
-	for (int i = 0; i < input.length(); i++) {
+	for (int i = 0; i < length / 2; i++) {
 		char temp = input[i];
-		input[i] = input[length - i];
-		input[length - i] = temp;
+		input[i] = input[length - i - 1];
+		input[length - i - 1] = temp;
 	}
 
-	return input;
+	//Secondly, reverse every word in the string
 
+	return input;
 }
