b15e272f9ee92f4e902aa8d53a9f40e83f2fa098
Josh Varty <joshvarty@gmail.com>
2017-06-11 02:15:46 -0700

    Naive implementation
    Wow, my tests were off...

--- AlgorithmDesignManual/ArrayShifter.cpp
+++ AlgorithmDesignManual/ArrayShifter.cpp
@@ -1,6 +1,16 @@
 #include "ArrayShifter.h"
 
 
-std::vector<int> ArrayShifter::Shift(std::vector<int> input, int k) {
+std::vector<int> ArrayShifter::Shift(std::vector<int> input, int numShifts) {
+
+	for (int j = 0; j < numShifts; j++) {
+
+		int last = input[0];
+		for (int i = input.size() - 1; i >= 0; i--) {
+			int temp = input[i];
+			input[i] = last;
+			last = temp;
+		}
+	}
 	return input;
 }
--- AlgorithmDesignManual/ArrayShifterTests.cpp
+++ AlgorithmDesignManual/ArrayShifterTests.cpp
@@ -38,8 +38,8 @@ TEST(ArrayShifterTests, ThreeItems_TwoShifts)
 	auto result = ArrayShifter::Shift(input, 2);
 
 	EXPECT_EQ(result[0], 3);
-	EXPECT_EQ(result[1], 2);
-	EXPECT_EQ(result[2], 1);
+	EXPECT_EQ(result[1], 1);
+	EXPECT_EQ(result[2], 2);
 }
 
 TEST(ArrayShifterTests, ThreeItems_FiveShifts)
@@ -47,21 +47,21 @@ TEST(ArrayShifterTests, ThreeItems_FiveShifts)
 	std::vector<int> input = { 1, 2, 3 };
 	auto result = ArrayShifter::Shift(input, 5);
 
-	EXPECT_EQ(result[0], 2);
-	EXPECT_EQ(result[1], 3);
-	EXPECT_EQ(result[2], 1);
+	EXPECT_EQ(result[0], 3);
+	EXPECT_EQ(result[1], 1);
+	EXPECT_EQ(result[2], 2);
 }
 
 TEST(ArrayShifterTests, FiveItems_TwoShifts)
 {
 	std::vector<int> input = { 1, 2, 3, 4, 5};
-	auto result = ArrayShifter::Shift(input, 5);
+	auto result = ArrayShifter::Shift(input, 2);
 
 	EXPECT_EQ(result[0], 3);
 	EXPECT_EQ(result[1], 4);
 	EXPECT_EQ(result[2], 5);
 	EXPECT_EQ(result[3], 1);
-	EXPECT_EQ(result[4], 1);
+	EXPECT_EQ(result[4], 2);
 }
 
 #pragma endregion
