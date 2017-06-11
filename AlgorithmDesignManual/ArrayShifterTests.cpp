d3d043df0878ac5d110691eeb1e1354ce3988101
Josh Varty <joshvarty@gmail.com>
2017-06-11 02:00:27 -0700

    Correction.

--- AlgorithmDesignManual/ArrayShifterTests.cpp
+++ AlgorithmDesignManual/ArrayShifterTests.cpp
@@ -18,7 +18,8 @@ TEST(ArrayShifterTests, TwoItems_OneShift)
 	std::vector<int> input = { 1, 2 };
 	auto result = ArrayShifter::Shift(input, 1);
 
-	EXPECT_EQ(result, result);
+	EXPECT_EQ(result[0], 2);
+	EXPECT_EQ(result[1], 1);
 }
 
 TEST(ArrayShifterTests, TwoItems_TwoShifts)
@@ -26,7 +27,8 @@ TEST(ArrayShifterTests, TwoItems_TwoShifts)
 	std::vector<int> input = { 1, 2 };
 	auto result = ArrayShifter::Shift(input, 2);
 
-	EXPECT_EQ(result, result);
+	EXPECT_EQ(result[0], 1);
+	EXPECT_EQ(result[1], 2);
 }
 
 
@@ -35,7 +37,9 @@ TEST(ArrayShifterTests, ThreeItems_TwoShifts)
 	std::vector<int> input = { 1, 2, 3 };
 	auto result = ArrayShifter::Shift(input, 2);
 
-	EXPECT_EQ(result, result);
+	EXPECT_EQ(result[0], 3);
+	EXPECT_EQ(result[1], 2);
+	EXPECT_EQ(result[2], 1);
 }
 
 TEST(ArrayShifterTests, ThreeItems_FiveShifts)
@@ -43,7 +47,9 @@ TEST(ArrayShifterTests, ThreeItems_FiveShifts)
 	std::vector<int> input = { 1, 2, 3 };
 	auto result = ArrayShifter::Shift(input, 5);
 
-	EXPECT_EQ(result, result);
+	EXPECT_EQ(result[0], 2);
+	EXPECT_EQ(result[1], 3);
+	EXPECT_EQ(result[2], 1);
 }
 
 TEST(ArrayShifterTests, FiveItems_TwoShifts)
@@ -51,6 +57,11 @@ TEST(ArrayShifterTests, FiveItems_TwoShifts)
 	std::vector<int> input = { 1, 2, 3, 4, 5};
 	auto result = ArrayShifter::Shift(input, 5);
 
-	EXPECT_EQ(result, result);
+	EXPECT_EQ(result[0], 3);
+	EXPECT_EQ(result[1], 4);
+	EXPECT_EQ(result[2], 5);
+	EXPECT_EQ(result[3], 1);
+	EXPECT_EQ(result[4], 1);
 }
+
 #pragma endregion
