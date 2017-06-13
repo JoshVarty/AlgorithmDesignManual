f406cc14c20ed2e365a8249afc956cc6e8b2ca25
Josh Varty <joshvarty@gmail.com>
2017-06-13 01:37:36 -0700

    Initial implementation of Add

--- AlgorithmDesignManual/Trie.cpp
+++ AlgorithmDesignManual/Trie.cpp
@@ -1,7 +1,27 @@
 #include "Trie.h"
+#include <iostream>
+using namespace std;
 
 void Trie::Add(string input) {
+	this->count = this->count + 1;
+	
+	if (input.length() <= 0) {
+		return;
+	}
 
+	char leadingChar = input[0];
+	int index = leadingChar - 'a';
+	
+	if (this->characters[index] == nullptr) {
+		this->characters[index] = new Trie();
+	}
+
+	if (input.length() <= 1) {
+		return;
+	}
+
+	std::string remainder = input.substr(1, input.length() - 1);
+	this->characters[index]->Add(remainder);
 }
 
 int Trie::Find(string input) {
--- AlgorithmDesignManual/Trie.h
+++ AlgorithmDesignManual/Trie.h
@@ -4,6 +4,8 @@ using namespace std;
 
 class Trie {
 public:
+	int count;				//number of items that have followed this path
+	Trie * characters[26];	//my Trie only accepts lowercase english letters
 	void Add(string input);
 	int Find(string input);
 };
