d91584aea1a4355d5bb615aed15bfbac807c781d
Josh Varty <joshvarty@gmail.com>
2017-06-29 00:30:49 -0700

    Implement basic Longest Increasing Subsequence algorithm

--- AlgorithmDesignManual/LongestIncreasingSequence.cpp
+++ AlgorithmDesignManual/LongestIncreasingSequence.cpp
@@ -4,7 +4,24 @@ using namespace std;
 
 
 int getLengthOfLongestIncreasingSequence(int sequence[], int length) {
-	return -1;
+	int * lengths = new int[length];
+
+	for (int i = 0; i < length; i++) {
+		int currentItem = sequence[i];
+		int lengthOfSequence = 1;
+
+		for (int j = 0; j < i; j++) {
+			int item = sequence[j];
+			int lengthAtItem = lengths[j];
+			if (item < currentItem && lengthAtItem >= lengthOfSequence) {
+				lengthOfSequence = lengthAtItem + 1;
+			}
+		}
+
+		lengths[i] = lengthOfSequence;
+	}
+
+	return lengths[length - 1];
 }
 
 
