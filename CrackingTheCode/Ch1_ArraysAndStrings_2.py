8f10a2674ca1f10846caa17937cab7a99a1253e4
Josh Varty <joshvarty@gmail.com>
2018-01-04 00:37:15 -0500

    Early termination solution

--- CrackingTheCode/Ch1_ArraysAndStrings_2.py
+++ CrackingTheCode/Ch1_ArraysAndStrings_2.py
@@ -11,6 +11,9 @@ def isPermutation1(str1, str2):
 
 
 def isPermutation2(str1, str2):
+    
+    if len(str1) != len(str2):
+        return False
 
     seen_chars = [0 for _ in range(128)]
 
@@ -28,7 +31,23 @@ def isPermutation2(str1, str2):
 
     return True
 
+def isPermutation3(str1, str2):
+    if len(str1) != len(str2):
+        return False
 
+    seen_chars = [0 for _ in range(128)]
+
+    for char in str1:
+        charVal = ord(char)
+        seen_chars[charVal] = seen_chars[charVal] + 1
+
+    for char in str2:
+        charVal = ord(char)
+        if seen_chars[charVal] == 0:
+            return False;
+        seen_chars[charVal] = seen_chars[charVal] - 1
+ 
+    return True
 
 assert(isPermutation1("", "") == True)
 assert(isPermutation1("josh", "josh") == True)
@@ -45,3 +64,11 @@ assert(isPermutation2("josh", "shoj") == True)
 assert(isPermutation2("josh", "joshj") == False)
 assert(isPermutation2("josh", "josj") == False)
 assert(isPermutation2("josh", "") == False)
+
+assert(isPermutation3("", "") == True)
+assert(isPermutation3("josh", "josh") == True)
+assert(isPermutation3("josh", "oshj") == True)
+assert(isPermutation3("josh", "shoj") == True)
+assert(isPermutation3("josh", "joshj") == False)
+assert(isPermutation3("josh", "josj") == False)
+assert(isPermutation3("josh", "") == False)
