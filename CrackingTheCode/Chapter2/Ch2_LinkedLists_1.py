a96008cc1cf5bc3472ebc578ca7e137156a9f314
Josh Varty <joshvarty@gmail.com>
2018-01-14 17:39:22 -0500

    Another solution for O(1) memory.

--- CrackingTheCode/Chapter2/Ch2_LinkedLists_1.py
+++ CrackingTheCode/Chapter2/Ch2_LinkedLists_1.py
@@ -19,8 +19,24 @@ def removeDuplicates_Buffer(head):
     return head.toList()
 
 def removeDuplicates_NoSet(head):
-    pass
+    #For each node, remove all nodes after it that have the same value
+    current = head
+    while current is not None and current.next is not None:
+        currentData = current.data
+        removeDataFromList(current, currentData)
+        current = current.next
 
+    return head.toList()
+
+
+def removeDataFromList(current, data):
+
+    while current.next is not None:
+        nextData = current.next.data
+        if nextData == data:
+            current.next = current.next.next
+        else:
+            current = current.next
 
 #NoRemovals
 head = Node(1)
@@ -45,3 +61,44 @@ head.appendToTail(1)
 head.appendToTail(3)
 head.appendToTail(2)
 assert(removeDuplicates_Buffer(head) == [1,2,3])
+
+#Dupes at end
+head = Node(1)
+head.appendToTail(2)
+head.appendToTail(3)
+head.appendToTail(4)
+head.appendToTail(4)
+assert(removeDuplicates_Buffer(head) == [1,2,3,4])
+
+
+#NoRemovals
+head = Node(1)
+head.appendToTail(2)
+head.appendToTail(3)
+head.appendToTail(4)
+head.appendToTail(5)
+assert(removeDuplicates_NoSet(head) == [1,2,3,4,5])
+
+#All dupes
+head = Node(1)
+head.appendToTail(1)
+head.appendToTail(1)
+head.appendToTail(1)
+head.appendToTail(1)
+assert(removeDuplicates_NoSet(head) == [1])
+
+#Multiple dupes
+head = Node(1)
+head.appendToTail(2)
+head.appendToTail(1)
+head.appendToTail(3)
+head.appendToTail(2)
+assert(removeDuplicates_NoSet(head) == [1,2,3])
+
+#Dupes at end
+head = Node(1)
+head.appendToTail(2)
+head.appendToTail(3)
+head.appendToTail(4)
+head.appendToTail(4)
+assert(removeDuplicates_NoSet(head) == [1,2,3,4])
