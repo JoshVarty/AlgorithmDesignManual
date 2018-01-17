60f083db9ea9e71202eadd8166546de0c0b4f703
Josh Varty <joshvarty@gmail.com>
2018-01-17 15:15:34 -0500

    Solution for 2.6

---
+++ CrackingTheCode/Chapter2/Ch2_LinkedLists_6.py
@@ -0,0 +1,111 @@
+#Palindrome: Implement a function to check if a linked list is a palindrome.
+
+from Node import Node
+
+
+def isPalindromeWithStack(head):
+
+    if head is None or head.next is None:
+        return True
+
+    stack = []
+    fast = head
+    slow = head
+
+    while fast is not None and fast.next is not None:
+        stack.insert(0, slow.data)
+        slow = slow.next
+        fast = fast.next.next
+
+    #If it was an odd length list, we skip the middle item
+    if fast is not None:
+        slow = slow.next
+
+    #Now continue moving slow, making sure all match what we've put in our stack
+    while slow is not None:
+
+        top = stack[0]
+        if slow.data != top:
+            return False
+
+        stack.remove(top)
+        slow = slow.next
+
+    return True 
+
+
+head = Node(1)
+result = isPalindromeWithStack(head)
+assert(result == True)
+
+head = Node(1)
+head.appendToTail(1)
+result = isPalindromeWithStack(head)
+assert(result == True)
+
+head = Node(1)
+head.appendToTail(1)
+head.appendToTail(1)
+result = isPalindromeWithStack(head)
+assert(result == True)
+
+head = Node(1)
+head.appendToTail(5)
+head.appendToTail(1)
+result = isPalindromeWithStack(head)
+assert(result == True)
+
+head = Node(1)
+head.appendToTail(5)
+head.appendToTail(5)
+head.appendToTail(1)
+result = isPalindromeWithStack(head)
+assert(result == True)
+
+head = Node(1)
+head.appendToTail(5)
+head.appendToTail(9)
+head.appendToTail(5)
+head.appendToTail(1)
+result = isPalindromeWithStack(head)
+assert(result == True)
+
+
+head = Node(1)
+head.appendToTail(2)
+result = isPalindromeWithStack(head)
+assert(result == False)
+
+head = Node(0)
+head.appendToTail(1)
+head.appendToTail(1)
+result = isPalindromeWithStack(head)
+assert(result == False)
+
+head = Node(1)
+head.appendToTail(5)
+head.appendToTail(3)
+result = isPalindromeWithStack(head)
+assert(result == False)
+
+head = Node(1)
+head.appendToTail(5)
+head.appendToTail(9)
+head.appendToTail(1)
+result = isPalindromeWithStack(head)
+assert(result == False)
+
+head = Node(1)
+head.appendToTail(2)
+head.appendToTail(9)
+head.appendToTail(5)
+head.appendToTail(1)
+result = isPalindromeWithStack(head)
+assert(result == False)
+
+#node = head.appendToTail(2)
+#head.appendToTail(3)
+#head.appendToTail(4)
+#head.appendToTail(5)
+#head.appendToTail(6)
+#head.appendToTail(7)
