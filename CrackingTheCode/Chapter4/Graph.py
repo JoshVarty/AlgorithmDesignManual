ed4fc818bd865f4efed0e81e6d7c3beff4df0f23
Josh Varty <joshvarty@gmail.com>
2018-01-19 14:30:05 -0500

    Let's just let this node work for trees too.

--- CrackingTheCode/Chapter4/Graph.py
+++ CrackingTheCode/Chapter4/Graph.py
@@ -1,11 +1,43 @@
+
+
 class Graph:
     def __init__(nodes):
         self.nodes = nodes
 
 class Node:
-
     def __init__(self, value):
         self.data = value
+
+        #Can be used as either a binary tree or regular graph
         self.children = []
+        self.left = None
+        self.right = None
+
+def dfs(node, list = None):
+
+    if list is None:
+        list = []
+        dfs(node, list)
+        return list
+
+    if node.left is not None:
+        dfs(node.left, list)
+   
+    list.append(node.data)
+
+    if node.right is not None:
+        dfs(node.right, list)
+
+
+
+node = Node(4)
+node.left = Node(2)
+node.right = Node(6)
+node.left.left = Node(1)
+node.left.right = Node(3)
+node.right.left = Node(5)
+node.right.right = Node(7)
+result = dfs(node)
+assert(result == [1,2,3,4,5,6,7])
 
 
