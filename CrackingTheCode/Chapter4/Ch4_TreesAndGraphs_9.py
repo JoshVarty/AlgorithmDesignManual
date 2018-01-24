558106239a1c099d60f9f25f79e5322d220004a9
Josh Varty <joshvarty@gmail.com>
2018-01-24 12:35:43 -0500

    Code to generate interleaved permutations

--- CrackingTheCode/Chapter4/Ch4_TreesAndGraphs_9.py
+++ CrackingTheCode/Chapter4/Ch4_TreesAndGraphs_9.py
@@ -2,8 +2,10 @@
 # search tree with distinct elements, print all possible arrays that could have led to this tree 
 
 
-
 def generatePermutations(nums, lowIdx):
+    """
+    Vanilla permutation of a list of elements
+    """
 
     permutations = []
     if lowIdx == len(nums) - 1:
@@ -35,3 +37,43 @@ assert(result[1] == [2,1])
 nums = [1,2,3]
 result = generatePermutations(nums, 0)
 assert(len(result) == 6)
+
+
+def permutateInterleavings(nums1, nums2, idx1 = 0, idx2 = 0, prefix = []):
+
+    left = []
+    right = []
+
+    permutations = []
+
+    if idx1 >= len(nums1):
+        return [prefix + nums2[idx2:]]
+    
+    if idx2 >= len(nums2):
+        return [prefix + nums1[idx1:]]
+
+    leftPrefix = prefix + [nums1[idx1]]
+    left = permutateInterleavings(nums1, nums2, idx1 + 1, idx2, leftPrefix)
+
+    rightPrefix = prefix + [nums2[idx2]]
+    right = permutateInterleavings(nums1, nums2, idx1, idx2 + 1, rightPrefix)
+
+    left.extend(right)
+    return left
+
+
+
+result = permutateInterleavings([1], [2])
+assert(len(result) == 2)
+
+result = permutateInterleavings([1,2], [3])
+assert(len(result) == 3)
+
+result = permutateInterleavings([1,2], [3,4])
+assert(len(result) == 6)
+
+result = permutateInterleavings([1,2,3], [4,5])
+assert(len(result) == 10)
+
+result = permutateInterleavings([1,2,3], [4,5,6])
+assert(len(result) == 20)
