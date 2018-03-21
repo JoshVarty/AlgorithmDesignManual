f367acd404ee4e57d641bc4faff685b3f65be286
Josh Varty <joshvarty@gmail.com>
2018-03-21 12:24:41 -0400

    Solution for 17.05

---
+++ CrackingTheCode/Chapter17/Ch17_Moderate_05.py
@@ -0,0 +1,36 @@
+# Letters and Numbers: Given an array filled with letters and numbers, find the longest subarray with an equal number of letters and numbers
+
+def longestSubarray(input):
+
+    differences = [0]
+
+    for idx in range(len(input)):
+        char = input[idx]
+
+        prevValue = differences[idx]
+
+        if char.isdigit():
+            differences.append(prevValue  - 1)
+        else:
+            differences.append(prevValue  + 1)
+
+    #Now we're looking for the furthest apart equivalent values
+
+    lookup = {0:-1}
+    maxPair = (0, 0)
+    for idx, delta in differences:
+        if delta not in lookup:
+            lookup[delta] = idx
+        else:
+            match = lookup[delta]
+            distance = idx - match
+            longest = maxPair[1] - maxPair[0]
+            if distance > longest:
+                max[0] = match
+                max[1] = idx
+
+
+    return maxPair
+
+longestSubarray("a0a0000")
+
